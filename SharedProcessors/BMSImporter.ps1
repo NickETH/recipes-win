73e59241e25d1a41909940f451f159bc4bf45ad3
Heim  Niklaus (ID BD) <heim@ethz.ch>
2020-05-23 15:10:39 +0200

    New BMSImporter processor

---
+++ SharedProcessors/BMSImporter.ps1
@@ -0,0 +1,250 @@
+# Create a new BMS Application
+
+# PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%cd%\BMSImporter.ps1'"
+# Todo: Logfile based on the presence of a bms_imp_logfile parameter
+
+param(
+    [Parameter(mandatory=$true)][string]$bms_serverurl,
+    [Parameter(mandatory=$true)][string]$bms_serverport,
+    [Parameter(mandatory=$true)][string]$bms_CM_entry,
+    [Parameter(mandatory=$true)][string]$bms_username,
+    [Parameter(mandatory=$true)][string]$bms_app_name,
+    [Parameter(mandatory=$true)][string]$bms_app_vendor,
+    [Parameter(mandatory=$true)][string]$bms_app_parentid,
+    [Parameter(mandatory=$true)][string]$bms_app_version,
+    [Parameter(mandatory=$true)][string]$bms_app_valid4os,
+    [Parameter(mandatory=$true)][string]$bms_app_seccont,
+    [Parameter(mandatory=$true)][string]$bms_app_installcmd,
+    [Parameter(mandatory=$false)][string]$bms_app_installparm,
+    [Parameter(mandatory=$false)][string]$bms_app_iopt_rebootbhv,
+    [Parameter(mandatory=$false)][string]$bms_app_iopt_copylocal,
+    [Parameter(mandatory=$false)][string]$bms_app_iopt_reinstall,
+    [Parameter(mandatory=$false)][string]$bms_app_iopt_target,
+    [Parameter(mandatory=$false)][string]$bms_app_comment,
+    [Parameter(mandatory=$false)][string]$bms_app_category,
+    [Parameter(mandatory=$false)][string]$bms_app_conschecks,
+    [Parameter(mandatory=$false)][string]$bms_app_uninstcmd,
+    [Parameter(mandatory=$false)][string]$bms_app_uninstparm,
+	[Parameter(mandatory=$false)][string]$bms_app_localfilecopy,
+    [Parameter(mandatory=$false)][string]$bms_app_dependencies,
+    [Parameter(mandatory=$false)][string]$inst_file_src_dest,
+    [Parameter(mandatory=$false)][string]$read_file_src_dest,
+    [Parameter(mandatory=$false)][string]$bms_imp_logfile
+);
+
+$logfile = ("C:\Tools\AutoPKG\log\BMSimporter" + $bms_app_name + (Get-Date -Format yyyyMMdd-HHmm) + ".txt")
+
+echo "----------Start of BMSimporter--------------" | Out-File $logfile -Append
+echo ("bms_server: " + $bms_serverurl + " bms_appname: " + $bms_app_name) | Out-File $logfile -Append
+# Convert a boolean string to a bool type value.
+function ParseBool{
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0)]
+        [System.String]$inputVal
+    )
+    switch -regex ($inputVal.Trim())
+    {
+        "^(1|true|yes|on|enabled)$" { $true }
+
+        default { $false }
+    }
+}
+Import-Module bConnect
+# Do we need to load this assemblies?
+[Windows.Security.Credentials.PasswordVault,Windows.Security.Credentials,ContentType=WindowsRuntime]
+$vault = New-Object Windows.Security.Credentials.PasswordVault
+
+#Allow the usage of different encryption methods.
+#https://stackoverflow.com/questions/11696944/powershell-v3-invoke-webrequest-https-error
+$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+
+#$bms_CM_entry = "AutoPKG-BMSImporter"
+if (-Not ($storedCredential = $vault.Retrieve($bms_CM_entry, $bms_username))){
+    $objCred = Get-Credential -UserName $bms_username -Message "Authentifizierung am Baramundi-Server:"
+    # Convert credential to appropriate type and store in vault
+    $credObject = New-Object Windows.Security.Credentials.PasswordCredential -ArgumentList ($bms_CM_entry, $objCred.UserName,$objCred.GetNetworkCredential().Password)
+    $vault.Add($credObject)
+}
+
+# Retrieve the credential from vault
+$storedCredential = $vault.Retrieve($bms_CM_entry, $bms_username)
+# Create a PSCredential object to give to bConnect
+$objCred = New-Object System.Management.Automation.PSCredential -ArgumentList ($storedCredential.UserName, (ConvertTo-SecureString $storedCredential.Password -AsPlainText -Force))
+
+Initialize-bConnect -Server $bms_serverurl -Port $bms_serverport -Credentials $objCred
+<#
+echo "InstallOptions" | Out-File $logfile -Append
+echo "bms_app_iopt_copylocal: " + $bms_app_iopt_copylocal | Out-File $logfile -Append
+echo "bms_app_iopt_rebootbhv: " + $bms_app_iopt_rebootbhv | Out-File $logfile -Append
+#>
+
+# Check for an existing application with the same name and version.
+# If an application with same name and version exists in the staging OU, we will remove it.
+# If an application with same name and version exists in another OU, we can not alter it and have to stop here.
+# We are searching trough all the applications on this server! Eye on perfomance! Alternative: a parameter array with the OU's to search...
+# $All_Apps_in_OU = Get-bConnectApplication -OrgUnitGuid $bms_app_parentid
+$All_Apps_in_BMS = Get-bConnectApplication
+foreach ($Application in $All_Apps_in_BMS) {
+    if ($Application.Name -eq $bms_app_name -and $Application.Version -eq $bms_app_version -and $Application.ParentId -eq $bms_app_parentid){
+        Remove-bConnectApplication -ApplicationGuid $Application.Id
+        echo "An application with the same name and version was removed from the staging OU!" | Out-File $logfile -Append
+    }
+    Elseif ($Application.Name -eq $bms_app_name -and $Application.Version -eq $bms_app_version -and $Application.ParentId -ne $bms_app_parentid){
+        echo "An application with the same name and version is already in production!" | Out-File $logfile -Append
+        Exit
+        #[Environment]::Exit(0)
+    }
+}
+
+# Create the options hash tables for install and uninstall
+if ($bms_app_iopt_rebootbhv -or $bms_app_iopt_copylocal -or $bms_app_iopt_reinstall -or $bms_app_target) {
+    $InstallOptArgs = @{ }
+    echo "InstallOptions inside If" | Out-File $logfile -Append
+    echo ("bms_app_iopt_copylocal in If: " + $bms_app_iopt_copylocal) | Out-File $logfile -Append
+
+    # echo @InstallOptArgs | Out-File $logfile -Append
+    if ($bms_app_iopt_rebootbhv) { 
+		$InstallOptArgs['RebootBehaviour'] = $bms_app_iopt_rebootbhv
+		# uninstall options are quick and dirty implemented here, because there is just this one for now.
+		# if there will be more, this has to be relocated...
+		$UnInstallOptArgs = @{ }
+		$UnInstallOptArgs['RebootBehaviour'] = $bms_app_iopt_rebootbhv
+		$UnInstallOptions = New-bConnectApplicationInstallOptions @UnInstallOptArgs
+	}
+    if ($bms_app_iopt_copylocal ) { $InstallOptArgs['CopyLocally'] = (ParseBool($bms_app_iopt_copylocal)) }
+    if ($bms_app_iopt_reinstall ) { $InstallOptArgs['AllowReinstall'] = (ParseBool($bms_app_iopt_reinstall)) }
+    if ($bms_app_iopt_target) { $InstallOptArgs['Target'] = $bms_app_iopt_target }
+    # echo @InstallOptArgs | Out-File $logfile -Append
+    $InstallOptions = New-bConnectApplicationInstallOptions @InstallOptArgs
+    echo @InstallOptArgs | Out-File $logfile -Append
+    echo "1" | Out-File $logfile -Append
+}
+
+$installDataArgs = @{
+    Command	   = $bms_app_installcmd;
+}
+if ($bms_app_installparm) { $installDataArgs['Parameter'] = $bms_app_installparm }
+if ($InstallOptions) { $installDataArgs['Options'] = $InstallOptions }
+$InstallationData = New-bConnectApplicationInstallationData @installDataArgs
+echo @InstallationData | Out-File $logfile -Append
+echo "2" | Out-File $logfile -Append
+
+if ($bms_app_uninstcmd -or $bms_app_uninstparm) {
+    $UnInstallDataArgs = @{
+            Command	   = $bms_app_uninstcmd;
+    }
+    if ($bms_app_uninstparm) { $UnInstallDataArgs['Parameter'] = $bms_app_uninstparm }
+	if ($UnInstallOptions) { $UnInstallDataArgs['Options'] = $UnInstallOptions }
+    $UninstallationData = New-bConnectApplicationInstallationData @UnInstallDataArgs
+    echo @UninstallationData | Out-File $logfile -Append
+    echo "3" | Out-File $logfile -Append
+}
+
+
+#Create an array for the "ValidForOS" object.
+$Val4OSary = $bms_app_valid4os.Split(",") | where-object {$_ -ne " "}
+
+echo ("Valid4OS-before: " + $bms_app_valid4os) | Out-File $logfile -Append
+echo ("Valid4OS-after: " + $Val4OSary) | Out-File $logfile -Append
+
+$AppInstallArgs = @{
+    Name             = $bms_app_name;
+    Vendor           = $bms_app_vendor;
+    Version          = $bms_app_version;
+    ValidForOS       = $Val4OSary;
+    ParentId         = $bms_app_parentid;
+    SecurityContext  = $bms_app_seccont;
+    InstallationData = $InstallationData;
+}
+
+#A bunch of optional settings
+if ($bms_app_category) { $AppInstallArgs['Category'] = $bms_app_category }
+if ($bms_app_comment) { $AppInstallArgs['Comment'] = $bms_app_comment }
+if ($UninstallationData) { $AppInstallArgs['UninstallationData'] = $UninstallationData }
+if ($bms_app_conschecks) { $AppInstallArgs['ConsistencyChecks'] = $bms_app_conschecks }
+
+#If we have a file to copy locally
+if ($bms_app_localfilecopy) {
+	#For now, only a single file is implemented
+	#To extend it to an array, use dependencies as example.
+    $File_obj_local = New-bConnectApplicationFile -Source $bms_app_localfilecopy -Type "File"
+	$App_localFile_Arj = @()
+	$App_localFile_Arj += @( [PSCustomObject]$File_obj_local)
+	if ($File_obj_local) { $AppInstallArgs['Files'] = $App_localFile_Arj }
+	
+	echo ("Local File: " + $bms_app_localfilecopy) | Out-File $logfile -Append
+}
+
+#If we have dependencies
+if ($bms_app_dependencies) {
+    $fldr_seprtr = "|||"
+    $split_optn = [System.StringSplitOptions]::RemoveEmptyEntries
+    $depend_file_ary = $bms_app_dependencies.Split($fldr_seprtr,$split_optn)
+    $App_Depend_Arj = @()
+    $fldr_seprtr = "~~~"
+    foreach ($depend_file in $depend_file_ary) {
+        $depend_file_opt = $depend_file.Split($fldr_seprtr,$split_optn)
+
+        foreach ($Application in $All_Apps_in_BMS) {
+            if ($Application.Name -eq $depend_file_opt[0] -and $Application.Version -eq $depend_file_opt[1]){
+                $App_Dep = @{
+                    DependencyId      = $Application.Id;
+                    DependencyAppName = $Application.Name;
+                    DependencyType    = [PSCustomObject]"InstallBeforeIfNotInstalled";
+                    ValidForOS        = [PSCustomObject]$Val4OSary
+                }
+    			$Depend_Data = New-bConnectApplicationDependency @App_Dep
+                $App_Depend_Arj += @( [PSCustomObject]$Depend_Data)
+            }
+        }
+        echo ("Dependency " + $Application.Name) | Out-File $logfile -Append
+    }
+    $AppInstallArgs['SoftwareDependencies'] = $App_Depend_Arj
+}
+
+echo @AppInstallArgs | Out-File $logfile -Append
+echo "Just before New-App" | Out-File $logfile -Append
+<#
+#>
+echo @AppInstallArgs | Out-File $logfile -Append
+
+New-bConnectApplication @AppInstallArgs
+
+#Copy the install file(s) to the destination storage location
+if ($inst_file_src_dest) {
+    $fldr_seprtr = "|"
+    $split_optn = [System.StringSplitOptions]::RemoveEmptyEntries
+    $inst_file_ary = $inst_file_src_dest.Split($fldr_seprtr,$split_optn)
+    if( -Not (Test-Path -Path $inst_file_ary[1])){
+        New-Item -Type dir $inst_file_ary[1]
+    }
+    echo ("inst_file_src_dest: " + ($inst_file_ary[0] + "  " + $inst_file_ary[1]))
+    if ($inst_file_ary[0] -match '\*') {
+        Copy-Item -Force -Recurse $inst_file_ary[0] $inst_file_ary[1]
+    }
+    else{
+        Copy-Item -Force $inst_file_ary[0] $inst_file_ary[1]
+    }
+}
+
+#Copy the readme/create-log file(s) to the destination storage location
+if ($read_file_src_dest) {
+    $fldr_seprtr = "|"
+    $split_optn = [System.StringSplitOptions]::RemoveEmptyEntries
+    $read_file_ary = $read_file_src_dest.Split($fldr_seprtr,$split_optn)
+    if( -Not (Test-Path -Path $read_file_ary[1])){
+        New-Item -Type dir $read_file_ary[1]
+    }
+    echo ("read_file_src_dest: " + ($read_file_ary[0] + "  " + $read_file_ary[1]))
+    if ($read_file_ary[0] -match '\*') {
+        Copy-Item -Force -Recurse $read_file_ary[0] $read_file_ary[1]
+    }
+    else{
+        Copy-Item -Force $read_file_ary[0] $read_file_ary[1]
+    }
+
+}
+echo ("----------End of BMSimporter -- bms_server: " + $bms_serverurl + " bms_appname: " + $bms_app_name + " --------------")| Out-File $logfile -Append
+
---
+++ SharedProcessors/BMSImporter.py
@@ -0,0 +1,262 @@
+#!/usr/bin/python
+#
+# Copyright 2019 Swiss federal institute of technology (ETHZ).
+#
+# Created by Nick Heim (heim)@ethz.ch) on 2019-04-26.
+#
+# Import a new application into BMS (baramundi management system).
+# Depends on the powershell script BMSImporter.ps1.
+# Calls the BMS REST API to create a new application.
+# Copies the files to the destination network shares.
+# Extended with port/CM-entry, ApplicationFile and Dependencies, 20200523, Hm
+
+import os
+import sys
+import subprocess
+
+from autopkglib import Processor, ProcessorError
+
+
+__all__ = ["BMSImporter"]
+
+
+class BMSImporter(Processor):
+    description = "Import a new application into BMS."
+    input_variables = {
+        "bms_serverurl": {
+            "required": True,
+            "description": "URL of the BMS server, required",
+        },
+        "bms_serverport": {
+            "required": False,
+            "description": "Port of the BMS bConnect service, Defaults to: 443",
+        },
+        "bms_CM_entry": {
+            "required": True,
+            "description": "Name of the object in the credential manager, required",
+        },
+        "bms_username": {
+            "required": True,
+            "description": "Username to log into the BMS bconnect API, required",
+        },
+        "bms_app_name": {
+            "required": True,
+            "description": "Application name in BMS, required",
+        },
+        "bms_app_vendor": {
+            "required": True,
+            "description": "Application vendor name in BMS, required",
+        },
+        "bms_app_parentid": {
+            "required": True,
+            "description": "GUID of the Parent OU in BMS, required",
+        },
+        "bms_app_version": {
+            "required": True,
+            "description": "Application version in BMS, required",
+        },
+        "bms_app_valid4os": {
+            "required": True,
+            "description": "Valid OS for the application in BMS, required",
+        },
+        "bms_app_seccont": {
+            "required": True,
+            "description": "Application's security context in BMS, required",
+        },
+        "bms_app_installcmd": {
+            "required": True,
+            "description": "Application install command line in BMS, required",
+        },
+        "bms_app_installparm": {
+            "required": False,
+            "description": "Application install parameters in BMS.",
+        },
+        "bms_app_iopt_rebootbhv": {
+            "required": False,
+            "description": "Application install option <reboot behaviour> in BMS.",
+        },
+        "bms_app_iopt_copylocal": {
+            "required": False,
+            "description": "Application install option <copy locally> in BMS.",
+        },
+        "bms_app_iopt_reinstall": {
+            "required": False,
+            "description": "Application install option <Reinstallation allowed> in BMS.",
+        },
+        "bms_app_iopt_target": {
+            "required": False,
+            "description": "Application install option <target> in BMS.",
+        },
+        "bms_app_comment": {
+            "required": False,
+            "description": "Application comment in BMS.",
+        },
+        "bms_app_conschecks": {
+            "required": False,
+            "description": "Application consistency check in BMS.",
+        },
+        "bms_app_uninstcmd": {
+            "required": False,
+            "description": "Application uninstall command line in BMS.",
+        },
+        "bms_app_uninstparm": {
+            "required": False,
+            "description": "Application uninstall parameters in BMS.",
+        },
+        "bms_app_localfilecopy": {
+            "required": False,
+            "description": "File to copy locally.",
+        },
+        "bms_app_dependencies": {
+            "required": False,
+            "description": "(Array of) 'Name~~~Version' of a dependency.",
+        },
+        "inst_file_src_dest": {
+            "required": False,
+            "description": "Application install file(s) to copy to the DIP-Share, use wildcards for multiple objects.",
+        },
+        "read_file_src_dest": {
+            "required": False,
+            "description": "Application readme and/or create-log file(s) to copy to the DIP-Share, use wildcards for multiple objects.",
+        },
+        "bms_imp_logfile": {
+            "required": False,
+            "description": "Path to a logfile for exensive logging of the importer.",
+        },
+        "ignore_errors": {
+            "required": False,
+            "description": "Ignore any errors during the run.",
+        },
+    }
+    output_variables = {}
+
+    __doc__ = description
+
+    def main(self):
+
+        bms_serverurl = self.env.get('bms_serverurl')
+        bms_serverport = self.env.get('bms_serverport', "443")
+        bms_CM_entry = self.env.get('bms_CM_entry')
+        bms_username = self.env.get('bms_username')
+        bms_app_name = self.env.get('bms_app_name')
+        bms_app_vendor = self.env.get('bms_app_vendor')
+        bms_app_parentid = self.env.get('bms_app_parentid')
+        bms_app_version = self.env.get('bms_app_version')
+        bms_app_valid4os = self.env.get('bms_app_valid4os')
+        bms_app_seccont = self.env.get('bms_app_seccont')
+        bms_app_installcmd = self.env.get('bms_app_installcmd')
+        ignore_errors = self.env.get('ignore_errors', True)
+        verbosity = self.env.get('verbose', 5)
+        print >> sys.stdout, "bms_app_name %s" % bms_app_name
+
+        self.output("Creating: %s" % bms_app_name)
+        sharedproc_dir = os.path.dirname(os.path.realpath(__file__))
+        ps_script = os.path.join(sharedproc_dir, 'BMSImporter.ps1')
+        powershell = "C:\\windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe"
+        # Call the powershell script with its arguments.
+        cmd = [powershell, '-NoProfile', '-ExecutionPolicy', 'Bypass', '-File',
+            ps_script,
+            bms_serverurl,
+            bms_serverport,
+            bms_CM_entry,
+            bms_username,
+            bms_app_name,
+            bms_app_vendor,
+            bms_app_parentid,
+            bms_app_version,
+            bms_app_valid4os,
+            bms_app_seccont,
+            bms_app_installcmd,]
+
+        if "bms_app_installparm" in self.env:
+            bms_app_installparm = self.env.get('bms_app_installparm')
+            cmd.extend(['-bms_app_installparm', bms_app_installparm])
+
+        if "bms_app_iopt_rebootbhv" in self.env:
+            bms_app_iopt_rebootbhv = self.env.get('bms_app_iopt_rebootbhv')
+            cmd.extend(['-bms_app_iopt_rebootbhv', bms_app_iopt_rebootbhv])
+
+        if "bms_app_iopt_copylocal" in self.env:
+            bms_app_iopt_copylocal = self.env.get('bms_app_iopt_copylocal')
+            cmd.extend(['-bms_app_iopt_copylocal', bms_app_iopt_copylocal])
+
+        if "bms_app_iopt_reinstall" in self.env:
+            bms_app_iopt_reinstall = self.env.get('bms_app_iopt_reinstall')
+            cmd.extend(['-bms_app_iopt_reinstall', bms_app_iopt_reinstall])
+
+        if "bms_app_iopt_target" in self.env:
+            bms_app_iopt_target = self.env.get('bms_app_iopt_target')
+            cmd.extend(['-bms_app_iopt_target', bms_app_iopt_target])
+
+        if "bms_app_comment" in self.env:
+            bms_app_comment = self.env.get('bms_app_comment')
+            cmd.extend(['-bms_app_comment', bms_app_comment])
+
+        if "bms_app_category" in self.env:
+            bms_app_category = self.env.get('bms_app_category')
+            cmd.extend(['-bms_app_category', bms_app_category])
+
+        if "bms_app_conschecks" in self.env:
+            bms_app_conschecks = self.env.get('bms_app_conschecks')
+            cmd.extend(['-bms_app_conschecks', bms_app_conschecks])
+
+        if "bms_app_uninstcmd" in self.env:
+            bms_app_uninstcmd = self.env.get('bms_app_uninstcmd')
+            cmd.extend(['-bms_app_uninstcmd', bms_app_uninstcmd])
+
+        if "bms_app_uninstparm" in self.env:
+            bms_app_uninstparm = self.env.get('bms_app_uninstparm')
+            cmd.extend(['-bms_app_uninstparm', bms_app_uninstparm])
+
+        if "bms_app_localfilecopy" in self.env:
+            bms_app_localfilecopy = self.env.get('bms_app_localfilecopy')
+            cmd.extend(['-bms_app_localfilecopy', bms_app_localfilecopy])
+
+        if "bms_app_dependencies" in self.env:
+            bms_app_dependencies = self.env.get('bms_app_dependencies')
+            # if recipe writer gave us a single string instead of a list of strings,
+            # convert it to a list of strings
+            if isinstance(self.env["bms_app_dependencies"], basestring):
+                self.env["bms_app_dependencies"] = [self.env["bms_app_dependencies"]]
+
+            cmd_string = ""
+            for app_ver_entry in self.env["bms_app_dependencies"]:
+                cmd_string += app_ver_entry + '|||'
+			
+            cmd.extend(['-bms_app_dependencies', cmd_string])
+
+        if "inst_file_src_dest" in self.env:
+            inst_file_src_dest = self.env.get('inst_file_src_dest')
+            cmd.extend(['-inst_file_src_dest', inst_file_src_dest])
+
+        if "read_file_src_dest" in self.env:
+            read_file_src_dest = self.env.get('read_file_src_dest')
+            cmd.extend(['-read_file_src_dest', read_file_src_dest])
+
+        if "bms_imp_logfile" in self.env:
+            bms_imp_logfile = self.env.get('bms_imp_logfile')
+            cmd.extend(['-bms_imp_logfile', bms_imp_logfile])
+
+        # print >> sys.stdout, "cmdline %s" % cmd
+        try:
+            if verbosity > 1:
+                Output = subprocess.check_output(cmd)
+            else:
+                Output = subprocess.check_output(cmd)
+        except:
+            if ignore_errors != 'True':
+                raise
+        print >> sys.stdout, "cmdline Output %s" % Output
+        #self.env['pkg_dir'] = Output
+		
+        archiveVersion = ""
+        for line in Output.split("\n"):
+            if verbosity > 2:
+                print line
+            if "Buildversion:" in line:
+                archiveVersion = line.split()[-1]
+                continue
+
+if __name__ == '__main__':
+    processor = BMSImporter()
+    processor.execute_shell()
