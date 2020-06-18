d5b2fa39e29ab5b6b190c42e60778f3d1dcacaa5
Heim  Niklaus (ID BD) <heim@ethz.ch>
2020-06-18 17:13:09 +0200

    MiKTeX recipe and ExecuteFile SP elevated update

---
+++ MiKTeX/MiKTeX-Win64.build/read/readme-MiKTeX_64_VVeerrssiioonn_EN.txt
@@ -0,0 +1,8 @@
+
+	MiKTeX x64 for Windows 1.0.0 EN
+	Will be installed to C:\APPS\MiKTeX by default.
+	Full installation.
+	
+	Release 1.0.0, Build 1
+
+20190923, by ETHZ ID-CD AutoPkg
--- SharedProcessors/ExecuteFile.py
+++ SharedProcessors/ExecuteFile.py
@@ -5,20 +5,45 @@
 # Created by Nick Heim (heim)@ethz.ch) on 2020-05-31.
 #
 # Execute a file.
-# MSBuild.exe [Optionen] [Projektdatei]
-# MSBuild MyApp.csproj -t:Clean -p:Configuration=Debug;TargetFrameworkVersion=v3.5
+# With the "elevated" flag, it is possible to run commands that would otherwise raise an elevation popup.
+# This function needs the AutoPkg Taskrunner utility, which is only available from v2.x
 
 from __future__ import absolute_import
 from __future__ import print_function
 import os
 import sys
+import time
 import subprocess
+# import ctypes
+import _winreg
 
 from autopkglib import Processor, ProcessorError
 
 
 __all__ = ["ExecuteFile"]
 
+#https://stackoverflow.com/questions/15008758/parsing-boolean-values-with-argparse
+def checkbool(v):
+    # makes checking a bool argument a lot easier...
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+    # end of checkbool()
+
+def reg_read(path, root=_winreg.HKEY_CURRENT_USER):
+    path, name = os.path.split(path)
+    with _winreg.OpenKey(root, path) as key:
+        return _winreg.QueryValueEx(key, name)[0]
+
+def reg_write(path, value, root=_winreg.HKEY_CURRENT_USER):
+    path, name = os.path.split(path)
+    with _winreg.OpenKey(root, path, 0, _winreg.KEY_WRITE) as key:
+        _winreg.SetValueEx(key, name, 0, _winreg.REG_SZ, value)
 
 class ExecuteFile(Processor):
     description = "Run an executable file (with arguments) (in a given directory)."
@@ -35,6 +60,11 @@ class ExecuteFile(Processor):
             "required": False,
             "description": "(Array of) argument(s) to the command line",
         },
+        "run_elevated": {
+            "default": False,
+            "required": False,
+            "description": "Run the EXE with elevated priviliges",
+        },
         "ignore_errors": {
             "required": False,
             "description": "Ignore any errors during the extraction.",
@@ -47,13 +77,11 @@ class ExecuteFile(Processor):
 
     def main(self):
 
-        exe_file = self.env.get('exe_file')
-        cmd = self.env.get('exe_file')
         ignore_errors = self.env.get('ignore_errors', True)
         verbosity = self.env.get('verbose', 1)
-
+        exe_file = self.env.get('exe_file')
+        print("exe_file: %s" % exe_file)
         cmd = [exe_file.encode('utf-8')]
-        # cmd = [exe_file]
 
         if "exe_folder" in self.env:
             exe_folder = self.env.get('exe_folder')
@@ -68,24 +96,39 @@ class ExecuteFile(Processor):
             if isinstance(self.env["cmdline_args"], basestring):
                 self.env["cmdline_args"] = [self.env["cmdline_args"]]
 
+            cmd_args = [''.encode('utf-8')]
             for arg_cmnd in self.env["cmdline_args"]:
-                cmd.extend([arg_cmnd.encode('utf-8')])
-                # print >> sys.stdout, "cmd %s" % cmd
+                cmd_args.extend([arg_cmnd.encode('utf-8')])
+                print("cmd_args: %s" % cmd_args)
 
-        print("cmdline %s" % (' '.join(cmd)), file=sys.stdout)
-        # print >> sys.stdout, "run_folder %s" % os.getcwd()
-        try:
-            if verbosity > 1:
-                #Output = subprocess.check_output(cmd)
-                #Output = subprocess.Popen(' '.join(cmd))
-                Output = subprocess.check_output(' '.join(cmd))
+        if "run_elevated" in self.env:
+            run_elevated = self.env.get('run_elevated')
+            print("run_elevated: %s" % run_elevated)
+            if checkbool(run_elevated):
+                try:
+                    comnd_value = ''.join(cmd) + '~~~' + (' '.join(cmd_args)).lstrip()
+                    taskrunnerpath = 'Software\AutoPkg\TaskRunner\Command'
+                    print("TaskRunnerCommand: %s" % comnd_value)
+                    reg_write(taskrunnerpath, comnd_value)
+                except BaseException as err:
+                    raise ProcessorError(
+                        "Can't write command to %s: %s" % (taskrunnerpath, err)
+                    )
+                while (reg_read(taskrunnerpath) != "0"):
+                    time.sleep(5)
+
             else:
-                #Output = subprocess.check_output(cmd)
-                Output = subprocess.Popen(' '.join(cmd))
-        except:
-            if ignore_errors != 'True':
-                raise
-                print("Execution result %s" % Output, file=sys.stdout)
+                print("cmdline %s" % (' '.join(cmd + cmd_args)), file=sys.stdout)
+                try:
+                    if verbosity > 1:
+                        Output = subprocess.check_output(' '.join(cmd + cmd_args))
+
+                    else:
+                        Output = subprocess.check_output(' '.join(cmd + cmd_args))
+                except:
+                    if ignore_errors != 'True':
+                        raise
+                        print("Execution result %s" % Output, file=sys.stdout)
 
 if __name__ == '__main__':
     processor = ExecuteFile()
